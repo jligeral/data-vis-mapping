f08888267edf653980f0512b5757eb15f87585b1
Tevin Takata <takatatevin19@gmail.com>
2025-12-05 18:21:40 -1000

    Added breaks between lines

--- web/aboutContent.js
+++ web/aboutContent.js
@@ -23,16 +23,20 @@ export const aboutContent = `
   <p>This is an interactive map of academic research in the field of information visualization. It allows you to explore nearly X papers from major conferences (like IEEE VIS, CHI, EuroVis) by the semantic similarity of their ideas as revealed through their titles and abstracts.</p>
   <p>This project is inspired by Friso van Dijk, Marco Spruit, Chaïm van Toledo, and Matthieu Brinkhuis and their paper:</p>
   <p><a href="https://www.frisovandijk.com/research/pillars-of-privacy.pdf" target="_blank" rel="noopener noreferrer" style="text-decoration: underline; color: inherit;">"Pillars of Privacy: Identifying Core Theory in a Network Analysis of Privacy Literature" (2021)</a></p>
+  <br/>
 
   <h4><strong>How is the data processed?</strong></h4>
   <p>Data was collected from OpenAlex, which catalogs scientific papers and information about them such as their title, year published, authors, and so on. Data was cleaned by removing unnecessary columns and renaming required columns.</p>
+  <br/>
 
   <h4><strong>How are the texts converted for analysis?</strong></h4>
   <p>Embeddings are created using TF-IDF (Term Frequency-Inverse Document Frequency) using the abstracts of each paper. If a paper does not have an abstract, the title is used instead. Since the vectors contain numerous dimensions, we use UMAP (Uniform Manifold Approximation and Projection) to reduce the data into a 3D space, preserving the semantic relationships between papers</p>
+  <br/>
 
   <h4><strong>How are clusters generated?</strong></h4>
   <p>Clusters are generated using HDBSCAN (Hierarchical Density-Based Spatial Clustering of Applications with Noise), which groups similar papers together based on their proximity in the reduced 3D space. Each cluster represents a group of papers that share similar themes or topics.</p>
-  
+  <br/>
+
   <h4><strong>What is the -1 cluster?</strong></h4>
   <p>The -1 cluster represents noise or outliers in the data that do not fit well into any of the identified clusters. These papers may cover unique topics or have less common themes compared to the main clusters.</p>
 `;
